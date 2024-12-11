6f08b1131a7a650e0f212f4661191c989cb2ce62
felipefsantos <felipe@theoldpup.com>
2024-12-11 16:26:54 +0000

    Create repository

---
+++ src/lib/components/Highlights.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import TypeIt from 'typeit';
+
+	let container: HTMLDivElement;
+
+	onMount(() => {
+		new TypeIt(container, {
+			strings: [
+				'Full Stack Development',
+				'Bespoke Web Design',
+				'Data Driven SEO',
+				'Hybrid Mobile Apps',
+				'Accessible & Responsive Layouts'
+			]
+		});
+	});
+</script>
+
+<div bind:this={container} class="w-full flex-1 text-white"></div>
