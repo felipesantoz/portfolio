2d711057b23d01785871d975c24ab4edcb3bc0f4
felipefsantos <felipe@theoldpup.com>
2024-12-17 18:31:10 +0000

    [Navigation] Fix incorrect tab on reload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import Navigation from '$lib/components/Navigation.svelte';
+	import { onMount } from 'svelte';
 	import '../app.css';
 	let { children } = $props();
 	let selectedPageName = $state('Home');
 	let container: HTMLDivElement | undefined = $state();
 	let scrollTop = $state(0);
+	onMount(() => {
+		scrollTop = container?.scrollTop ?? 0;
+	});
 </script>
 
 <div
-	class="h-screen w-screen snap-y snap-proximity overflow-y-auto overflow-x-hidden scroll-smooth bg-main font-fira-mono text-white md:snap-mandatory"
+	class="bg-vulcan-950 h-screen w-screen snap-y snap-proximity overflow-y-auto overflow-x-hidden scroll-smooth font-fira-mono text-white md:snap-mandatory"
 	bind:this={container}
 	onscroll={() => (scrollTop = container?.scrollTop ?? 0)}
 >
