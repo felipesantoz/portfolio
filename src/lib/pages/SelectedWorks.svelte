9c79ab078cadb914f86b665022921f95323ff723
felipesantoz <felipe.santoz2013@gmail.com>
2024-12-21 16:24:14 +0000

    [Selected Works] Rough draft

--- src/lib/pages/SelectedWorks.svelte
+++ src/lib/pages/SelectedWorks.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
 	import BracketedHeader from '$lib/components/BracketedHeader.svelte';
+	import Carousel from '$lib/components/Carousel.svelte';
+	import InView from '$lib/components/InView.svelte';
 	import PageTemplate from '$lib/components/PageTemplate.svelte';
+	import Underlined from '$lib/components/Underlined.svelte';
+	import type { PageData } from '../../routes/$types';
+
+	let { data }: { data: PageData } = $props();
 </script>
 
 <PageTemplate
 	id="selected-works"
-	class="m-auto flex min-h-screen max-w-5xl snap-start flex-col gap-12 px-6 py-8 md:py-16"
+	class="m-auto flex h-screen max-w-5xl snap-start flex-col items-center gap-12 px-6 py-8 md:py-16"
 >
-	<BracketedHeader class="text-2xl md:text-5xl">selected-works</BracketedHeader>
-	<div class="grid grid-cols-2">
-		<div class="px-12">
-			<h2>Pawprint App</h2>
-		</div>
-		<div class="px-12">
-			<h2>Admin Dashboard</h2>
-		</div>
-	</div>
-</PageTemplate>
+	<BracketedHeader class="w-full text-2xl md:text-5xl">selected-works</BracketedHeader>
+
+	<div class="flex h-full w-full max-w-xl flex-col gap-8">
+		<InView><h2 class="text-lg md:pl-12 md:text-3xl"><Underlined>pawprint</Underlined></h2></InView>
+		<Carousel slideSrcs={data.pawprintImageSrcs} alt="pawprint screenshots" />
+	</div></PageTemplate
+>
