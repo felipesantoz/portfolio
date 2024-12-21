b9530b540ae708e94306b7c9928654f284418467
felipesantoz <felipe.santoz2013@gmail.com>
2024-12-21 16:24:14 +0000

    [Selected Works] add badges

---
+++ src/lib/components/Badge.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<span
+	class="inline-flex h-fit items-center rounded-md bg-orange-900 bg-opacity-10 px-2 py-1 text-xs font-medium text-orange-500 ring-1 ring-inset ring-orange-500/30"
+	>{@render children()}</span
+>
--- src/lib/pages/SelectedWorks.svelte
+++ src/lib/pages/SelectedWorks.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Badge from '$lib/components/Badge.svelte';
 	import BracketedHeader from '$lib/components/BracketedHeader.svelte';
 	import Carousel from '$lib/components/Carousel.svelte';
 	import InView from '$lib/components/InView.svelte';
@@ -15,8 +16,17 @@
 >
 	<BracketedHeader class="w-full text-2xl md:text-5xl">selected-works</BracketedHeader>
 
-	<div class="flex h-full w-full max-w-xl flex-col gap-8">
-		<InView><h2 class="text-lg md:pl-12 md:text-3xl"><Underlined>pawprint</Underlined></h2></InView>
+	<div class="flex h-full w-full max-w-2xl flex-col gap-10">
+		<div class="flex flex-col gap-4">
+			<InView><h2 class="text-lg md:text-3xl"><Underlined>pawprint</Underlined></h2></InView>
+			<div class="flex gap-2">
+				<Badge>TypeScript</Badge>
+				<Badge>React</Badge>
+				<Badge>Tailwind</Badge>
+				<Badge>Svelte</Badge>
+				<Badge>Next.js</Badge>
+			</div>
+		</div>
 		<Carousel slideSrcs={data.pawprintImageSrcs} alt="pawprint screenshots" />
 	</div></PageTemplate
 >
