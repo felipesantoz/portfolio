a4393d82a16f0198d15b7764ca91cc5b18ad5f05
felipesantoz <felipe.santoz2013@gmail.com>
2024-12-21 16:24:13 +0000

    [Selected Works] Add skeleton

---
+++ src/lib/pages/SelectedWorks.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import BracketedHeader from '$lib/components/BracketedHeader.svelte';
+</script>
+
+<div
+	id="selected-works"
+	class="m-auto flex min-h-screen max-w-5xl snap-start flex-col gap-12 px-6 py-8 md:py-16"
+>
+	<BracketedHeader class="text-2xl md:text-5xl">selected-works</BracketedHeader>
+	<div class="grid grid-cols-2">
+		<div class="px-12">
+			<h2>Pawprint App</h2>
+		</div>
+		<div class="px-12">
+			<h2>Admin Dashboard</h2>
+		</div>
+	</div>
+</div>
